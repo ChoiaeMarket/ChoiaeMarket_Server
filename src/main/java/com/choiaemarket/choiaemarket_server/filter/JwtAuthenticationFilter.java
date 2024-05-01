e0ccae45274438b88797d58ec023105bdd14e0da
nodb <scnoh02@naver.com>
2024-05-02 01:08:35 +0900

    add: jwt

---
+++ src/main/java/com/choiaemarket/choiaemarket_server/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,82 @@
+package com.choiaemarket.choiaemarket_server.filter;
+
+import java.io.IOException;
+
+import org.hibernate.grammars.hql.HqlParser.SecondContext;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.choiaemarket.choiaemarket_server.provider.JwtProvider;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        try {
+            String token = parseBearerToken(request);
+
+            if (token == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String email = jwtProvider.validate(token);
+
+            if (email == null) {    // SigningKey가 안맞거나 토큰 기간이 만료
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            // 정보가 담김
+            AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, null, AuthorityUtils.NO_AUTHORITIES);
+
+            // 세부정보 설정
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            // 컨텍스트에 등록
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            securityContext.setAuthentication(authenticationToken);
+
+            // 외부에서 사용할 수 있도록 Holder에 채움
+            SecurityContextHolder.setContext(securityContext);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseBearerToken(HttpServletRequest request){
+        String authorization = request.getHeader("Authorization");
+
+        boolean hasAuthorization = StringUtils.hasText(authorization);   // hasText : null, 길이 0, white-space인 경우 false 반환
+        if (!hasAuthorization) return null;
+
+        boolean isBearer = authorization.startsWith("Bearer ");
+        if (!isBearer) return null;
+
+        String token = authorization.substring(7);
+        return token;
+    }
+    
+}
