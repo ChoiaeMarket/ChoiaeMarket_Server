3652ee2bc0f92db1f8d3efa75ff6226ed9407633
nodb <scnoh02@naver.com>
2024-05-02 01:10:26 +0900

    add: 초기파일

---
+++ src/main/java/com/choiaemarket/choiaemarket_server/ChoiaemarketServerApplication.java
@@ -0,0 +1,13 @@
+package com.choiaemarket.choiaemarket_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoiaemarketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoiaemarketServerApplication.class, args);
+	}
+
+}
