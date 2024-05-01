a5f80bb7c86ffd8abacaa05ecd52ca9467b84945
nodb <scnoh02@naver.com>
2024-05-02 01:08:56 +0900

    add: dto

---
+++ src/main/java/com/choiaemarket/choiaemarket_server/dto/request/auth/SignUpRequestDto.java
@@ -0,0 +1,38 @@
+package com.choiaemarket.choiaemarket_server.dto.request.auth;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class SignUpRequestDto {
+    
+    @NotBlank @Email // 이메일 형식
+    private String email;
+
+    @NotBlank @Size(min=8, max=20)  // 크기 제한
+    private String password;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String nickname;
+
+    @NotBlank @Pattern(regexp="^[0-9]{11,13}$") // 정규 표현식
+    private String tel;
+
+    @NotBlank
+    private String gender;
+
+    @NotNull @AssertTrue
+    private Boolean agreedPersonal;
+}
