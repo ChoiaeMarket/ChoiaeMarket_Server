c6ef3f2feca17a5d5009a7871c25edeeea155b56
nodb <scnoh02@naver.com>
2024-05-15 23:15:15 +0900

    add: 로그인 유저 response DTO

---
+++ src/main/java/com/choiaemarket/choiaemarket_server/dto/response/user/GetSignInUserResponseDto.java
@@ -0,0 +1,36 @@
+package com.choiaemarket.choiaemarket_server.dto.response.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.choiaemarket.choiaemarket_server.common.ResponseCode;
+import com.choiaemarket.choiaemarket_server.common.ResponseMessage;
+import com.choiaemarket.choiaemarket_server.dto.response.ResponseDto;
+import com.choiaemarket.choiaemarket_server.entity.UserEntity;
+
+import lombok.Getter;
+
+@Getter
+public class GetSignInUserResponseDto extends ResponseDto {
+    
+    private String email;
+    private String nickname;
+    private String profileImage;
+
+    private GetSignInUserResponseDto(UserEntity userEntity){
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        this.email = userEntity.getEmail();
+        this.nickname = userEntity.getNickname();
+        this.profileImage = userEntity.getProfileImage();
+    }
+
+    public static ResponseEntity<GetSignInUserResponseDto> success(UserEntity userEntity){
+        GetSignInUserResponseDto result = new GetSignInUserResponseDto(userEntity);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistUser(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_USER, ResponseMessage.NOT_EXISTED_USER);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
+    }
+}
